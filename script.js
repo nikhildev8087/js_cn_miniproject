9e15a7e538219379737ce70b661f0c4c46261754
nikhildev8087 <nikhil.b@prometteur.in>
2022-06-03 19:00:08 +0530

    qr code project completed

--- script.js
+++ script.js
@@ -35,10 +35,59 @@ function fetchFile(url){
 
 }
 
+// ***********************************************************
+
+// mini porject 2 QR code scanner 
+const wrapper = document.querySelector(".wrapper"),
+form2 = wrapper.querySelector("#form2"),
+file2 = form2.querySelector("#file2"),
+infotext = form2.querySelector("#qrtext");
+// const qrtextarea = document.querySelector("#qrtextarea");
+const qrimg = document.getElementById("qrimg");
+const qrCloseBtn = document.getElementById("qrCloseBtn");
+const qrCopyBtn = document.getElementById("qrCopyBtn");
 
 
 
+function fetchRequest(formData, file){
+  fetch("http://api.qrserver.com/v1/read-qr-code/", {
+      method: "POST", body: formData
+  })
+  .then(res => res.json())
+  .then(result => {
+      result = result[0].symbol[0].data;
+      console.log(result);
+      //   infotext.innerText = "Upload QR Code to scan";
+      //   wrapper.classList.add("active");
+      wrapper.querySelector(".qrtextarea").innerText = result;
+      let form2 = document.getElementById("qrimg").src = URL.createObjectURL(file);
+      
+      console.log("this is a data",result);
+    })
+    
+}
 
+
+
+file2.addEventListener("change", e =>{
+    // e.preventDefault();
+    let file = e.target.files[0]; //getting use selected file
+    let formData = new FormData();   // creating new object 
+    formData.append("file", file);   //adding selected file to the new object
+    fetchRequest(formData, file);    
+    console.log("this is a file ",file);
+})
+
+//copy text button logic 
+qrCopyBtn.addEventListener("click", ()=>{
+    const qrtextarea = document.querySelector(".qrtextarea");
+    let text = qrtextarea.textContent;
+    // text.execCommand("copy");
+    navigator.clipboard.writeText(text);
+    console.log(text);
+})
+
+form2.addEventListener("click", () => file2.click());
 
 
 
